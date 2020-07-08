44f130bdb4b80b95861206d124e5a7989c427a96
woo-greencar <woo.greencar@gmail.com>
2020-07-08 09:04:11 +0900

    file io methods added

--- src/ListWritable.java
+++ src/ListWritable.java
@@ -1,23 +1,28 @@
-import java.io.ByteArrayOutputStream;
 import java.io.DataInput;
 import java.io.DataInputStream;
 import java.io.DataOutput;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListWritable implements Writable {
 
-        private List<Integer> buff = null;
+        private List<Integer> buff;
 
         public ListWritable() {
                 buff = new ArrayList<Integer>();
+        }
+
+        public Integer size() {
+                return buff.size();
+        }
+
+        public Integer get(Integer i) {
+                return buff.get(i);
         }
 
         public void add(int value) {
@@ -50,62 +55,4 @@ public class ListWritable implements Writable {
                         out.writeInt(num);
                 }
         }
-
-        public void readFromDisk(String filePath) {
-                try {
-                        FileInputStream fis = new FileInputStream(
-                            filePath);
-                        DataInputStream dis = new DataInputStream(fis);
-                        read(dis);
-                        dis.close();
-                        fis.close();
-
-                } catch (IOException e) {
-                        e.printStackTrace();
-                }
-        }
-
-        public void writeToDisk(String filePath) {
-                File file = new File(filePath);
-                try {
-
-                        if (!file.createNewFile()) {
-                                System.out.println("file already exists");
-
-                                if(file.delete()) {
-                                        System.out.println("file deleted");
-                                }
-                                if(file.createNewFile()) {
-                                        System.out.println("new file generated");
-                                }
-                        } else {
-                                System.out.println("new file generated");
-                        }
-                } catch (IOException e) {
-                        e.printStackTrace();
-                }
-
-                FileOutputStream fos = null;
-                try {
-                        fos = new FileOutputStream(filePath);
-                        DataOutputStream dos = new DataOutputStream(fos);
-                        write(dos);
-                        dos.close();
-                        fos.close();
-
-                } catch (IOException e) {
-                        e.printStackTrace();
-                }
-
-        }
-
-        private byte[] serialize(DataOutput out) {
-                return null;
-        }
-
-        private Object deserialize(DataInput in) {
-
-                return null;
-        }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,14 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutput;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 
 public class Main {
+
 
   public static void main(String[] args) {
     ListWritable listWritable = new ListWritable();
@@ -14,14 +19,24 @@ public class Main {
     listWritable.add(3);
     listWritable.add(999);
 
-    byte[] bytes = serialize(listWritable);
-    
-    ListWritable l = new ListWritable();
-    deserialize(bytes, l);
-    
-    for(int i = 0; i < l.size(); i++) {
-      System.out.println(l.get(i));
+//    byte[] bytes = serialize(listWritable);
+
+//    ListWritable l = new ListWritable();
+//    deserialize(bytes, l);
+//
+//    for (int i = 0; i < l.size(); i++) {
+//      System.out.println(l.get(i));
+//    }
+
+
+
+    writeToDisk("serialize.se", listWritable);
+    ListWritable w = (ListWritable)readFromDisk("serialize.se");
+
+    for(int i = 0; i < w.size(); i++) {
+      System.out.println(w.get(i));
     }
+
   }
 
   public static byte[] serialize(Writable w) {
@@ -35,7 +50,7 @@ public class Main {
     return out.toByteArray();
 
   }
-  
+
   public static void deserialize(byte[] bytes, Writable obj) {
     DataInputStream in = new DataInputStream(new ByteArrayInputStream(bytes));
     try {
@@ -43,5 +58,59 @@ public class Main {
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  public static void writeToDisk(String filePath, Writable w) {
+    File file = new File(filePath);
+    try {
+
+      if (!file.createNewFile()) {
+        System.out.println("file already exists");
+
+        if (file.delete()) {
+          System.out.println("file deleted");
+        }
+        if (file.createNewFile()) {
+          System.out.println("new file generated");
+        }
+      } else {
+        System.out.println("new file generated");
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    try {
+      FileOutputStream fos = new FileOutputStream(file);
+      fos.write(serialize(w));
+      fos.close();
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static Writable readFromDisk(String filePath) {
+    try {
+      File file = new File(filePath);
+      if(!file.exists()) {
+        System.out.println("file does not exist!");
+        return null;
+      }
+
+      byte[] bytes = Files.readAllBytes(file.toPath());
+      ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+      DataInputStream dis = new DataInputStream(bais);
+      ListWritable l = new ListWritable();
+      deserialize(bytes, l);
+      dis.close();
+
+      return l;
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return null;
   }
 }
